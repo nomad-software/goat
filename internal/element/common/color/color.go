af97475dbd2139188d5827b292e2ac3ef81c7e72
Gary Willoughby <dev@nomad.uk.net>
2023-11-01 11:17:40 +0000

    Updated common color methods.

--- internal/element/common/color/color.go
+++ internal/element/common/color/color.go
@@ -9,18 +9,21 @@ func (ele stub) GetID() string { return "" } // IGNORE
 
 // SetForegroundColor sets the foreground color.
 // See [element.color] for color names.
+// A hexadecimal string can be used too. e.g. #FFFFFF.
 func (ele stub) SetForegroundColor(c string) {
-	tk.Get().Eval("%s configure -foreground %s", ele.GetID(), c)
+	tk.Get().Eval("%s configure -foreground {%s}", ele.GetID(), c)
 }
 
 // SetBackgroundColor sets the background color.
 // See [element.color] for color names.
+// A hexadecimal string can be used too. e.g. #FFFFFF.
 func (ele stub) SetBackgroundColor(c string) {
-	tk.Get().Eval("%s configure -background %s", ele.GetID(), c)
+	tk.Get().Eval("%s configure -background {%s}", ele.GetID(), c)
 }
 
 // SetInsertColor sets the insert color.
 // See [element.color] for color names.
+// A hexadecimal string can be used too. e.g. #FFFFFF.
 func (ele stub) SetInsertColor(c string) {
-	tk.Get().Eval("%s configure -insertbackground %s", ele.GetID(), c)
+	tk.Get().Eval("%s configure -insertbackground {%s}", ele.GetID(), c)
 }
