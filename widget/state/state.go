3a01e48b103eb3b4a1fb0ead15f3c04d7b53dfa4
Gary Willoughby <dev@nomad.uk.net>
2023-11-02 20:07:58 +0000

    Added base widget object.

---
+++ widget/state/state.go
@@ -0,0 +1,15 @@
+package state
+
+const (
+	Active     = "active"
+	Disabled   = "disabled"
+	Focus      = "focus"
+	Pressed    = "pressed"
+	Selected   = "selected"
+	Background = "background"
+	Readonly   = "readonly"
+	Alternate  = "alternate"
+	Invalid    = "invalid"
+	Hover      = "hover"
+	Hidden     = "hidden" // Only applies to canvas widgets.
+)
