ad92c741a58532eb4bb9f7fc6bc49b86cb3ce848
Gary Willoughby <dev@nomad.uk.net>
2023-11-09 18:40:30 +0000

    Finished implemented text widget.

--- widget/text/text.go
+++ widget/text/text.go
@@ -18,8 +18,11 @@ type Text struct {
 // <<Modified>>
 // <<Selection>>
 //
+//go:generate go run ../../internal/tools/generate/main.go -recv=*Text -pkg=borderwidth
+//go:generate go run ../../internal/tools/generate/main.go -recv=*Text -pkg=color
+//go:generate go run ../../internal/tools/generate/main.go -recv=*Text -pkg=font
 //go:generate go run ../../internal/tools/generate/main.go -recv=*Text -pkg=height
-//go:generate go run ../../internal/tools/generate/main.go -recv=*Text -pkg=padding
+//go:generate go run ../../internal/tools/generate/main.go -recv=*Text -pkg=relief
 //go:generate go run ../../internal/tools/generate/main.go -recv=*Text -pkg=scrollbar
 //go:generate go run ../../internal/tools/generate/main.go -recv=*Text -pkg=width
 func New(parent element.Element) *Text {
@@ -111,4 +114,23 @@ func (el *Text) Paste() {
 // See scroll the context to show the specified line and character.
 func (el *Text) See(line, char int) {
 	tk.Get().Eval("%s see %d.%d", el.GetID(), line, char)
+}
+
+// SetPadding sets the padding.
+func (el *Text) SetPadding(p int) {
+	tk.Get().Eval("%s configure -padx %d -pady %d", el.GetID(), p, p)
+}
+
+// SetSelectForegroundColor sets the selection color.
+// See [option.color] for color names.
+// A hexadecimal string can be used too. e.g. #FFFFFF.
+func (el *Text) SetSelectForegroundColor(c string) {
+	tk.Get().Eval("%s configure -selectforeground {%s}", el.GetID(), c)
+}
+
+// SetSelectBackgroundColor sets the selection color.
+// See [option.color] for color names.
+// A hexadecimal string can be used too. e.g. #FFFFFF.
+func (el *Text) SetSelectBackgroundColor(c string) {
+	tk.Get().Eval("%s configure -selectbackground {%s}", el.GetID(), c)
 }
