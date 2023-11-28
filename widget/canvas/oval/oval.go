e219c9ddb668180aed52ed6b7051c7dff2104fdf
Gary Willoughby <dev@nomad.uk.net>
2023-11-28 19:57:10 +0000

    Added outline to canvas oval.

--- widget/canvas/oval/oval.go
+++ widget/canvas/oval/oval.go
@@ -12,6 +12,7 @@ const (
 //
 //go:generate go run ../../../internal/tools/generate/main.go -recv=*Oval -pkg=canvas/dash
 //go:generate go run ../../../internal/tools/generate/main.go -recv=*Oval -pkg=canvas/fill
+//go:generate go run ../../../internal/tools/generate/main.go -recv=*Oval -pkg=canvas/outline
 //go:generate go run ../../../internal/tools/generate/main.go -recv=*Oval -pkg=canvas/state
 //go:generate go run ../../../internal/tools/generate/main.go -recv=*Oval -pkg=canvas/tag
 //go:generate go run ../../../internal/tools/generate/main.go -recv=*Oval -pkg=canvas/width
