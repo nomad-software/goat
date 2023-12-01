8770ff4de6924a4d2a8a2fb027f229fd7936eb10
Gary Willoughby <dev@nomad.uk.net>
2023-12-01 20:06:13 +0000

    Added tag methods to all canvas items.

--- internal/widget/canvas/tag/tag.go
+++ internal/widget/canvas/tag/tag.go
@@ -14,3 +14,13 @@ func (el stub) SetTags(tags ...string) {
 	tagStr := strings.Join(tags, " ")
 	tk.Get().Eval("%s itemconfigure %s -tags [list %s]", el.GetParent().GetID(), el.GetID(), tagStr)
 }
+
+// DeleteTags delete the tags.
+func (el stub) DeleteTag(tag string) {
+	tk.Get().Eval("%s dtag %s {%s}", el.GetParent().GetID(), el.GetID(), tag)
+}
+
+// DeleteAllTags delete all tags.
+func (el stub) DeleteAllTags() {
+	tk.Get().Eval("%s itemconfigure %s -tags {}", el.GetParent().GetID(), el.GetID())
+}
--- widget/canvas/rectangle/rectangle_tag.gen.go
+++ widget/canvas/rectangle/rectangle_tag.gen.go
@@ -15,3 +15,13 @@ func (el *Rectangle) SetTags(tags ...string) {
 	tagStr := strings.Join(tags, " ")
 	tk.Get().Eval("%s itemconfigure %s -tags [list %s]", el.GetParent().GetID(), el.GetID(), tagStr)
 }
+
+// DeleteTags delete the tags.
+func (el *Rectangle) DeleteTag(tag string) {
+	tk.Get().Eval("%s dtag %s {%s}", el.GetParent().GetID(), el.GetID(), tag)
+}
+
+// DeleteAllTags delete all tags.
+func (el *Rectangle) DeleteAllTags() {
+	tk.Get().Eval("%s itemconfigure %s -tags {}", el.GetParent().GetID(), el.GetID())
+}
